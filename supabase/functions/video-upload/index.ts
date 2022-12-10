c218e20469a49ab15846413370dab21c7acf7a85
Luca Beetz <luca-beetz@tutanota.com>
2022-12-10 01:19:57 +0100

    add test data to video-upload EF

--- supabase/functions/video-upload/index.ts
+++ supabase/functions/video-upload/index.ts
@@ -3,14 +3,36 @@
 // This enables autocomplete, go to definition, etc.
 
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 console.log("Hello from Functions!")
 
 serve(async (req) => {
-  const { name } = await req.json()
+  const { video_url } = await req.json()
   const data = {
-    message: `Hello ${name}!`,
-  }
+    message: `Received ${video_url} ...`,
+  };
+
+  // Get supabase client
+  const supabase = createClient(
+    Deno.env.get("SUPABASE_URL") ?? "",
+    Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+    { global: { headers: { Authorization: req.headers.get("Authorization")! } } }
+  );
+
+  // Get transcription, title and thumbnail from api
+  const title = "CS50P - Lecture 5 - Unit Tests";
+  const thumbnail = "https://i.ytimg.com/vi/3Q_oYDQ2whs/maxresdefault.jpg";
+
+  // Save title and thumbnail to videos table
+  const upsertRes = await supabase
+    .from("videos")
+    .insert({ title, thumbnail, video_url })
+    .select("id");
+
+  console.log(upsertRes);
+
+  // Save transcription to storage
 
   return new Response(
     JSON.stringify(data),
